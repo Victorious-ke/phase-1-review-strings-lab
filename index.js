96d14e36276d3a07efb3fe662c5ccb3da5c7a771
Victorious Ngaruiya <victoriousngaruiya@gmail.com>
2025-07-26 06:44:33 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// const welcomeMessage = currentUser;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Short greeting with only the first letter of the user's name
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
